63b369ef37307fec869549b258ac11c87847a5ad
vidhangithub <vidhan.amc@gmail.com>
2021-08-15 12:06:13 +0100

    Spring Boot Skelleton code pushed

---
+++ src/main/java/com/vidhanfamilyservices/VidhanfamilyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vidhanfamilyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidhanfamilyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidhanfamilyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidhanfamilyservices/VidhanfamilyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhanfamilyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidhanfamilyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
